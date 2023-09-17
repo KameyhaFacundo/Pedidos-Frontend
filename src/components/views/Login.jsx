e850862ac4960fff3a362dbef2b9f11bc0baa7da
unknown <isaiaspadros2@gmail.com>
2023-09-17 16:33:30 -0300

    Agregado de forms en login, con su respectivo checkeo de entradas

--- src/components/views/Login.jsx
+++ src/components/views/Login.jsx
@@ -1,3 +1,10 @@
+import { Button, Card, Container, Form } from "react-bootstrap"
+import { login } from "../helpers/queries"
+import { useForm } from "react-hook-form"
+import Swal from "sweetalert2"
+import { useNavigate } from "react-router-dom"
+
+
 
 
 const onSubmit = (usuario)=>{
@@ -25,3 +32,61 @@ const onSubmit = (usuario)=>{
     })
 
   }
+
+  return (
+    <Container className="mainSection">
+      <Card className="my-5">
+        <Card.Header as="h5">Login</Card.Header>
+        <Card.Body>
+          <Form onSubmit={handleSubmit(onSubmit)}>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label>Email</Form.Label>
+              <Form.Control
+                type="email"
+                placeholder="Ingrese un email"
+                {
+                  ...register('email',{
+                    required: 'El email es un dato obligatorio',
+                    pattern:{
+                      value:/^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/,
+                      message:'El email debe cumplir con un formato valido como el siguiente mail@mail.com '
+                    }
+                  })
+                }
+              />
+              <Form.Text className="text-danger">
+                {errors.email?.message}
+              </Form.Text>
+             
+            </Form.Group>
+
+            <Form.Group className="mb-3" controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control
+                type="password"
+                placeholder="Password"
+               {
+                ...register('password',{
+                  required:'El password es un dato obligatorio',
+                  pattern: {
+                    value:/^(?=\w*\d)(?=\w*[A-Z])(?=\w*[a-z])\S{8,16}$/,
+                    message: 'el password debe tener al entre 8 y 16 caracteres, al menos un dígito, al menos una minúscula y al menos una mayúscula.'
+                  }
+                })
+               }
+              />
+             <Form.Text className="text-danger">
+                {errors.password?.message}
+              </Form.Text>
+            </Form.Group>
+            <Button variant="primary" type="submit">
+              Ingresar
+            </Button>
+          </Form>
+        </Card.Body>
+      </Card>
+    </Container>
+  );
+
+
+export default login;

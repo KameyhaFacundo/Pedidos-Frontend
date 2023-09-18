66d0525825e656c08a700c5c8d4bbebb54b5b95c
Rivadeneiea <joserivadeneira538@gmail.com>
2023-09-18 19:40:53 -0300

    agrego queries de manera local para hacer pruebas y quedo  esperando el link de backend

---
+++ src/components/helpers/queries.js
@@ -0,0 +1,65 @@
+const uriProducto = import.meta.env.VITE_API_PRODUCTO;
+
+export const listarProducto = async () => {
+  try {
+    const respuesta = await fetch(uriProducto);
+
+    const listaProductos = await respuesta.json();
+    return listaProductos;
+  } catch (error) {
+    console.log(error);
+  }
+};
+// POST
+export const crearProducto = async (producto) => {
+  try {
+    const resp = await fetch(uriProducto, {
+      method: "POST",
+
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+      body: JSON.stringify(producto),
+    });
+    return resp;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const obtenerProducto = async (id) => {
+  try {
+    const resp = await fetch(`${uriProducto}/${id}`);
+    const data = await resp.json();
+    return data;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const editarProducto = async (id, productoEditado) => {
+  try {
+    const resp = await fetch(`${uriProducto}/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(productoEditado),
+    });
+    return resp;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const borrarProductoAPI = async (id) => {
+  try {
+    const respuesta = await fetch(`${uriProducto}/${id}`, {
+      method: "DELETE",
+    });
+    return respuesta;
+  } catch (error) {
+    console.log(error);
+  }
+};

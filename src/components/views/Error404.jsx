16f2d4b4a2c5127a895b4a913b38ebd76330a049
unknown <isaiaspadros2@gmail.com>
2023-09-17 21:15:39 -0300

    Agregado de estilo a la pagina de error404

--- src/components/views/Error404.jsx
+++ src/components/views/Error404.jsx
@@ -1,15 +1,17 @@
+import "../../styles/error404.css"
 import { Button, Col, Container, Row } from 'react-bootstrap';
 import error from '../../assets/error404.png';
+
 
 const Error404 = () => {
     return (
         <Container>
-            <Row className="d-flex align-items-center justify-content-center" style={{ minHeight: '100vh' }}>
+            <Row className="d-flex align-items-center justify-content-center Min-vh">
                 <Col xs={12} md={6} className="text-center">
                    
                     <div className='mt-5'>
-                        <p style={{ fontSize: '52px' , marginBottom: '0'}}>ERROR 404 !</p>
-                        <p style={{ fontSize: '32px' }}>Page not found</p>
+                        <p className='Texto-G'>ERROR 404 !</p>
+                        <p className="Texto-S">Page not found</p>
                         <Button className='margin-left-4' variant="outline-danger">Return</Button>
                     </div>
                 </Col>

cb7d23a35cb47f07dc7977fc584b66ed095e4562
unknown <isaiaspadros2@gmail.com>
2023-09-20 09:25:26 -0300

    Agregado de funcion de mapeo para mostrar integrantes

--- src/components/views/AcercaDeNosotros.jsx
+++ src/components/views/AcercaDeNosotros.jsx
@@ -1,21 +1,29 @@
 const GrupoCards = ({ imageSrc, Apellido, Nombre, socialLinks }) => {
-    return (
-      
-      <div className="d-flex justify-content-center align-items-center" style={{ minHeight: '30vh' }}>
-      <Card style={{ width: '80%' }}>
+  return (
+    <div
+      className="d-flex justify-content-center align-items-center"
+      style={{ minHeight: "30vh" }}
+    >
+      <Card style={{ width: "80%" }}>
         <Row className="align-items-center">
           <Col sm={4}>
-            <Card.Img variant="top" src={imageSrc}  />
+            <Card.Img variant="top" src={imageSrc} />
           </Col>
           <Col sm={8}>
             <Card.Body className="d-flex flex-column justify-content-center align-items-center">
               <Card.Title className="mb-3">
-                  <span style={{ fontSize: '3.8rem' }}>{Apellido} {Nombre}</span>
-                  </Card.Title>
+                <span style={{ fontSize: "3.8rem" }}>
+                  {Apellido} {Nombre}
+                </span>
+              </Card.Title>
               <div className="d-flex flex-column">
                 {socialLinks.map((link, index) => (
                   <a key={index} href={link.url} className="mb-2">
-                    <img src={link.icon} alt={link.name} style={{ width: '50px', height: '50px' }} />
+                    <img
+                      src={link.icon}
+                      alt={link.name}
+                      style={{ width: "50px", height: "50px" }}
+                    />
                   </a>
                 ))}
               </div>
@@ -23,52 +31,113 @@ const GrupoCards = ({ imageSrc, Apellido, Nombre, socialLinks }) => {
           </Col>
         </Row>
       </Card>
+    </div>
+  );
+};
+
+const Participantes = () => {
+  const people = [
+    {
+      id: 1,
+      Apellido: "Padros",
+      Nombre: "Marcos Isaias",
+      imageSrc: Isaias,
+      socialLinks: [
+        { url: "https://github.com/IsaiasPadros", icon: GitIcono, name: "Git" },
+        { url: "", icon: Linkedin, name: "Linkedin" },
+      ],
+    },
+    {
+      id: 2,
+      Apellido: "Romo",
+      Nombre: "Mauro Agustin",
+      imageSrc: Mauro,
+      socialLinks: [
+        { url: "https://github.com/MauroRomo1", icon: GitIcono, name: "Git" },
+        {
+          url: "https://www.linkedin.com/in/mauro-romo-dharez/",
+          icon: Linkedin,
+          name: "Linkedin",
+        },
+      ],
+    },
+    {
+      id: 3,
+      Apellido: "Rivadeneira",
+      Nombre: "Jose Luis",
+      imageSrc: Jose,
+      socialLinks: [
+        { url: "https://github.com/Rivadeneiea", icon: GitIcono, name: "Git" },
+        { url: "", icon: Linkedin, name: "Linkedin" },
+      ],
+    },
+    {
+      id: 4,
+      Apellido: "Kameyha",
+      Nombre: "Facundo Adrian",
+      imageSrc: Facundo,
+      socialLinks: [
+        {
+          url: "https://github.com/KameyhaFacundo",
+          icon: GitIcono,
+          name: "Git",
+        },
+        { url: "", icon: Linkedin, name: "Linkedin" },
+      ],
+    },
+    {
+      id: 5,
+      Apellido: "Zelaya",
+      Nombre: "Sergio",
+      imageSrc: Sergio,
+      socialLinks: [
+        {
+          url: "https://github.com/SergioSZelaya",
+          icon: GitIcono,
+          name: "Git",
+        },
+        { url: "", icon: Linkedin, name: "Linkedin" },
+      ],
+    },
+    {
+      id: 6,
+      Apellido: "Geronimo",
+      Nombre: "Daniela",
+      imageSrc: Daniela,
+      socialLinks: [
+        {
+          url: "https://github.com/Danielageronimo",
+          icon: GitIcono,
+          name: "Git",
+        },
+        { url: "", icon: Linkedin, name: "Linkedin" },
+      ],
+    },
+  ];
+
+  return (
+    <>
+      <div className="text-center">
+        <h1 style={{ color: "var(--secondary-one-violet)" }}>GLP TEAM </h1>
       </div>
-    );
-  };
+      <div>
+        {people.map((person) => (
+          <div
+            key={person.id}
+            className="mb-4"
+            style={{ marginBottom: "20px" }}
+          >
+            <GrupoCards
+              Apellido={person.Apellido}
+              Nombre={person.Nombre}
+              imageSrc={person.imageSrc}
+              socialLinks={person.socialLinks}
+            />
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
-  const Participantes = () => {
-    const people = [
-      { id: 1, Apellido: 'Padros', Nombre: 'Marcos Isaias', imageSrc: Isaias, socialLinks: [
-        { url: 'https://github.com/IsaiasPadros', icon: GitIcono, name: 'Git' },
-        { url: '', icon: Linkedin, name: 'Linkedin' },
-        
-      ] },
-      { id: 2, Apellido: 'Romo', Nombre: 'Mauro Agustin', imageSrc: Mauro, socialLinks: [
-          { url: 'https://github.com/MauroRomo1', icon: GitIcono, name: 'Git' },
-          { url: 'https://www.linkedin.com/in/mauro-romo-dharez/', icon: Linkedin, name: 'Linkedin' },
-          
-        ] },
-        { id: 3, Apellido: 'Rivadeneira', Nombre: 'Jose Luis', imageSrc: Jose, socialLinks: [
-          { url: 'https://github.com/Rivadeneiea', icon: GitIcono, name: 'Git' },
-          { url: '', icon: Linkedin, name: 'Linkedin' },
-          
-        ] },
-        { id: 4, Apellido: 'Kameyha', Nombre: 'Facundo Adrian', imageSrc: Facundo, socialLinks: [
-          { url: 'https://github.com/KameyhaFacundo', icon: GitIcono, name: 'Git' },
-          { url: '', icon: Linkedin, name: 'Linkedin' },
-          
-        ] },
-        { id: 5, Apellido: 'Zelaya', Nombre: 'Sergio', imageSrc: Sergio, socialLinks: [
-          { url: 'https://github.com/SergioSZelaya', icon: GitIcono, name: 'Git' },
-          { url: '', icon: Linkedin, name: 'Linkedin' },
-          
-        ] },
-        { id: 5, Apellido: 'Geronimo', Nombre: 'Daniela', imageSrc: Daniela, socialLinks: [
-          { url: 'https://github.com/Danielageronimo', icon: GitIcono, name: 'Git' },
-          { url: '', icon: Linkedin, name: 'Linkedin' },
-          
-        ] }
-        
-      
-    ];
-    
-    
-    
-  
-  export default Participantes;
-  
-
-  
-  
-  
+export default Participantes;

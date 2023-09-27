389d9125efbc198f8fd11e92c42db889301ad519
Facundo Kameyha <kameyhafacundo@gmail.com>
2023-09-27 19:52:24 -0300

    Modifico funcion listProducts

--- src/components/helpers/queries.js
+++ src/components/helpers/queries.js
@@ -1,42 +1,12 @@
-const uriUsuario = import.meta.env.VITE_API_USUARIO;
-const uriProductos = import.meta.env.VITE_API_PRODUCTOS;
-
-const fetchData = async (url) => {
-  try {
-    const answer = await fetch(url);
-    const data = await answer.json();
-    return data;
-  } catch (error) {
-    console.error("Error:", error);
-  }
-};
-
-// export const login = async (user) => {
-//   try {
-//     const answer = await fetch(uriUsuario);
-//     const listUsers = await answer.json();
-
-//     const searchUser = listUsers.find(
-//       (itemUser) => itemUser.email === user.email
-//     );
-//     if (searchUser) {
-//       if (searchUser.password === user.password) {
-//         return searchUser;
-//       } else {
-//         return null;
-//       }
-//     } else {
-//       return null;
-//     }
-//   } catch (error) {
-//     console.log(error);
-//   }
-// };
-
-export const listUsers = async () => {
-  return fetchData(uriUsuario);
-};
+const uriProducts = import.meta.env.VITE_API_PRODUCTOS;
 
 export const listProducts = async () => {
-  return fetchData(uriProductos);
+  try {
+    const resp = await fetch(uriProducts);
+    const productList = await resp.json();
+    return productList;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
 };

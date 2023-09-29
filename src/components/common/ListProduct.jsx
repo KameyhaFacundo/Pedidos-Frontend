139667b6f83a303f095f1f18f7af25942c6558fb
Facundo Kameyha <kameyhafacundo@gmail.com>
2023-09-29 20:13:01 -0300

    Arreglo formato

--- src/components/common/ListProduct.jsx
+++ src/components/common/ListProduct.jsx
@@ -2,22 +2,18 @@ import { Col, Container, Row } from "react-bootstrap";
 import { allProducts } from "../../data/dataProducts";
 import CardProducto from "../views/producto/CardProducto";
 
-
 const ListProduct = () => {
-    
-     
-    return (
-        <Container>
-          <Row>
+  return (
+    <Container>
+      <Row>
+        {allProducts?.map((product) => (
+          <Col key={product.id}>
+            <CardProducto product={product} />
+          </Col>
+        ))}
+      </Row>
+    </Container>
+  );
+};
 
-          {allProducts?.map((product) => (
-            <Col key={product.id}>
-                <CardProducto product={product}/>
-            </Col>
-          ))}
-          </Row>
-        </Container>
-    );
-  };
-  
-  export default ListProduct;
+export default ListProduct;

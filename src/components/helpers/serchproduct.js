4ed1e91880e807b1fe79696f98c559caab8c1ab6
Facundo Kameyha <kameyhafacundo@gmail.com>
2023-09-30 13:30:30 -0300

    Se agrego formato

--- src/components/helpers/serchproduct.js
+++ src/components/helpers/serchproduct.js
@@ -1,8 +1,7 @@
 import { allProducts } from "../../data/dataProducts";
 
-
 export const searchProductId = (id) => {
-    const productoEncontrado = allProducts.find((producto) => producto.id === id);
-  
-    return productoEncontrado;
-  };
+  const productoEncontrado = allProducts.find((producto) => producto.id === id);
+
+  return productoEncontrado;
+};
